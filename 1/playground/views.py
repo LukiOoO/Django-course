2f136e09cd5f55d46d42b5bfca3d50c0b0cca41e
LukiOoO <leszczynskilukasz2003@gmail.com>
2024-04-20 13:21:53 +0200

    Push course to github

---
+++ 1/playground/views.py
@@ -0,0 +1,10 @@
+import re
+from urllib import request
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def say_hello(request):
+    return render(request, 'hello.html', {'name': 'luki'})
